27a938c5e29c40cb140c34d576e1145ba380d210
GiftWind <mvdeadsoul@gmail.com>
2016-11-02 15:41:04 +0300

    Add Forecast and Statistics

---
+++ HeadFirstDesignPatterns/Observer/ForecastDisplay.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Observer
+{
+    class ForecastDisplay : IObserver, IDisplayElement
+    {
+        private float _currentPressure = 768;
+        private float _lastPressure;
+        private WeatherData _weatherData;
+
+        public ForecastDisplay(WeatherData weatherData)
+        {
+            this._weatherData = weatherData;
+            weatherData.RegisterObserver(this);
+        }
+
+        public void Update(float temperature, float humidity, float pressure)
+        {
+            _lastPressure = _currentPressure;
+            _currentPressure = pressure;
+            Display();
+        }
+
+        public void Display()
+        {
+            Console.Write("Forecast: ");
+            if (_currentPressure > _lastPressure)
+            {
+                Console.WriteLine("Improving weather on the way!");
+            }
+            else if (Math.Abs(_currentPressure - _lastPressure) < 1)
+            {
+                Console.WriteLine("More of the same.");
+            }
+            else
+            {
+                Console.WriteLine("Watch out for cooler, rainy weather.");
+            }
+        }
+    }
+}
