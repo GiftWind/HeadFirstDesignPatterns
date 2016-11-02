a3821aaca325b956d72bf8bbae548fba1d74f25a
GiftWind <mvdeadsoul@gmail.com>
2016-11-02 14:24:12 +0300

    Implement Observer.

---
+++ HeadFirstDesignPatterns/Observer/CurrentConditionsDisplay.cs
@@ -0,0 +1,25 @@
+﻿namespace Observer
+{
+    class CurrentConditionsDisplay : IObserver, IDisplayElement
+    {
+        private float _temperature;
+        private float _humidity;
+        private ISubject _weatherData;
+
+        public CurrentConditionsDisplay(ISubject weatherData)
+        {
+            this._weatherData = weatherData;
+            weatherData.RegisterObserver(this);
+        }
+
+        public void Update(float temperature, float humidity, float pressure)
+        {
+            
+        }
+
+        public void Display()
+        {
+            
+        }
+    }
+}
