75902bbb67625921c3e55ba1e1c58d331b37c113
GiftWind <mvdeadsoul@gmail.com>
2016-11-08 15:17:47 +0300

    Add PizzaStore abstract class

---
+++ HeadFirstDesignPatterns/AbstractFactory/PizzaStore.cs
@@ -0,0 +1,19 @@
+﻿namespace AbstractFactory
+{
+    public abstract class PizzaStore
+    {
+        IPizzaIngredientFactory _ingredientFactory;
+        public Pizza OrderPizza(string type)
+        {
+            Pizza pizza = CreatePizza(type);
+            pizza.Prepare();
+            pizza.Bake();
+            pizza.Cut();
+            pizza.Box();
+
+            return pizza;
+        }
+
+        internal abstract Pizza CreatePizza(string type);
+    }
+}
