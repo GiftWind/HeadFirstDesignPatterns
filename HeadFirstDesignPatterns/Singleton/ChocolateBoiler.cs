9765b7c27b2db71250ef8dfa19bfaa7dcc924a7e
GiftWind <mvdeadsoul@gmail.com>
2016-11-08 16:04:21 +0300

    Implement naive Singleton.

---
+++ HeadFirstDesignPatterns/Singleton/ChocolateBoiler.cs
@@ -0,0 +1,49 @@
+﻿namespace Singleton
+{
+    class ChocolateBoiler
+    {
+        private bool _isEmpty;
+        private bool _isBoiled;
+        private static ChocolateBoiler _boiler;
+
+        private ChocolateBoiler()
+        {
+            _isEmpty = true;
+            _isBoiled = false;
+        }
+
+        public static ChocolateBoiler GetBoiler()
+        {
+            if (_boiler == null)
+            {
+                _boiler = new ChocolateBoiler();
+            }
+            return _boiler;
+        }
+
+        public void Fill()
+        {
+            if (_isEmpty)
+            {
+                _isEmpty = false;
+                _isBoiled = false;
+            }
+        }
+
+        public void Drain()
+        {
+            if (!_isEmpty && _isBoiled)
+            {
+                _isEmpty = true;
+            }
+        }
+
+        public void Boil()
+        {
+            if (!_isEmpty && !_isBoiled)
+            {
+                _isBoiled = true;
+            }
+        }
+    }
+}
