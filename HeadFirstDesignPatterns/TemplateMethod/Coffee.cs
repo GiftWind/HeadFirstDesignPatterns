d43152e3cfffb23bf6ad18a721970755ed24daaa
GiftWind <mvdeadsoul@gmail.com>
2016-11-16 18:39:39 +0300

    Add Beverages classes.

---
+++ HeadFirstDesignPatterns/TemplateMethod/Coffee.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace TemplateMethod
+{
+    public class Coffee
+    {
+        public void PrepareRecipe()
+        {
+            BoilWater();
+            BrewCoffeeGrinds();
+            PourInCup();
+            AddSugarAndMilk();
+        }
+
+        void BoilWater()
+        {
+            Console.WriteLine("Boiling water");
+        }
+
+        void BrewCoffeeGrinds()
+        {
+            Console.WriteLine("Dripping Coffee through filter");
+        }
+
+        void PourInCup()
+        {
+            Console.WriteLine("Pouring ino cup");
+        }
+
+        void AddSugarAndMilk()
+        {
+            Console.WriteLine("Adding Sugar and Milk");
+        }
+    }
+}
---
+++ HeadFirstDesignPatterns/TemplateMethod/Tea.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace TemplateMethod
+{
+    public class Tea
+    {
+        public void PrepareRecipe()
+        {
+            BoilWater();
+            SteepTeaBag();
+            PourInCup();
+            AddLemon();
+        }
+
+        void BoilWater()
+        {
+            Console.WriteLine("Boiling water");
+        }
+
+        void SteepTeaBag()
+        {
+            Console.WriteLine("Steeping the tea");
+        }
+
+        void PourInCup()
+        {
+            Console.WriteLine("Pouring into cup");
+        }
+
+        void AddLemon()
+        {
+            Console.WriteLine("Adding Lemon");
+        }
+    }
+}
